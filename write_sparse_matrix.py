d45f75d4d4e6100be300a5ebfd44aff96728dda9
fkunneman <f.kunneman@let.ru.nl>
2015-09-16 16:19:27 +0200

    fix

--- write_sparse_matrix.py
+++ write_sparse_matrix.py
@@ -9,9 +9,9 @@ infile = sys.argv[1]
 outdir = sys.argv[2]
 
 def write_sparse(dh, outdir):
-    tokenized = [' '.join([t[0] for t in instance]) + '\n' for instance in dh.dataset['tagged']])
+    tokenized = [' '.join([t[0] for t in instance]) + '\n' for instance in dh.dataset['tagged']]
     cn = featurizer.CocoNgrams([1,2,3],[])
-    cn.fit(tmp, tokenized)
+    cn.fit(outdir, tokenized)
     i, v = cn.transform(write = outdir + 'sparse.txt')
     with open(outdir + 'vocabulary.txt', 'w', encoding = 'utf-8') as v_out:
         v_out.write('\n'.join(v))
